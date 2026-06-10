cd8240262e1cbcfa7fe828af2bfc4cbb8b628e45
shiva12102006 <shiva12102006@gmail.com>
2026-06-10 11:54:46 +0530

    after last change

--- script.js
+++ script.js
@@ -214,3 +214,16 @@ document.addEventListener('keydown', (e) => {
         lightbox.classList.remove('active');
     }
 });
+
+const tabBtns = document.querySelectorAll(".tab-btn");
+const tabContents = document.querySelectorAll(".tab-content");
+
+tabBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    tabBtns.forEach((b) => b.classList.remove("active"));
+    tabContents.forEach((content) => content.classList.remove("active"));
+
+    btn.classList.add("active");
+    document.getElementById(btn.dataset.tab).classList.add("active");
+  });
+});
